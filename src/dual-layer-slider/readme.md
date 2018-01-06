054f851f37d7a0d23703b1424ad25fc8f8e33821
Mark Drake <markusdrake@gmail.com>
2018-01-06 18:57:47 -0500

    new component: dual layer slider

--- src/dual-layer-slider/readme.md
+++ src/dual-layer-slider/readme.md
@@ -7,14 +7,44 @@
 
 I first spotted this UI design in practice on the [Google reCAPTCHA website](https://www.google.com/recaptcha/intro/). Given the nature of UI / UX design and how rapidly websites will refresh I've captured the design and uploaded it to [YouTube](http://www.youtube.com/watch?v=bktcZFpFFp4).
 
-## Follow Up Improvements
+Why I like this particular design:
+
+1. When viewing this component I found myself primarily focused on the content. The content, any call to actions, and all the UI controls are presented to me in a nice tight box. I don't wonder about the page looking for anything. My focus is where it belongs.
+
+2. Simultaneously - we have the opportunity to present media in the background to grab our user's attention, drive some sort of emotion, even aid the user's understanding.
+
+3. Moving the background image and the text at the same time had some visual appeal to me and just looked like this would be a fun exercise!
+
+## Have Fun!
+
+### Changes I Made
+
+- I don't like to have target areas, that is an area that has a listener for a user event, less than 44px. This magic number originated from Apple's HUIG and has sort of stuck with me. One way I ensure the target area follows this guideline, even if the image itself is smaller, is by adding padding.
+
+- Google appears to be relying on JavaScript for applying width & height, transitioning between scenes, etc. Here are some minor differences:
+
+  - We rely on CSS for the animation behind our `transition` . It's triggerred by JavaScript, but CSS should provide better performance here. I'm not sure why they are animating the `left` position?
+
+  - We use a trick with `inline-block` and `white-space` rules to lineup our scenes. We don't use JavaScript to calculate and set the width and height. More importantly, we don't have to run this calculation between window resizes or get intermediary moments of FoUC.
+
+Just a quick note about my typography styling:
+
+- By default I apply a reset to `<h1>` through `<h6>` tags and instead control how they display by applying the appropriate CSS class. This benefits content authors as they only need to worry about the semantics and document outline, and aesthetics can stay the same.
+
+### Changes You Might Make
+
+Here are some final thoughts from me about the **Dual Layer Slider**:
 
 - I prefer to use `<img>` tags over backgrounds when possible, but especially when the image really adds to the content around it. Think about an ecommerce store showing product information, or helpful instructions, etc. It's nice to have this cataloged by search engines, and we can now give the image some `alt` text. Wins all around!
 
   - [`Object-Fit` has pretty good support in current generation browsers](https://caniuse.com/#feat=object-fit). There are polyfills and fallbacks available for older browsers that should help you to move away from this background-based solution.
 
-- Consider using the `<picture>` element, or the `srcset` & `sizes` attributes to (1) take advantage of new file formats that have better compression, and (2) excercise control over how small or large the requested image is, and lastly (3) apply some art direction in response to the viewport. [Read more about responsive images on MDN](https://developer.mozilla.org/en-US/docs/Learn/HTML/Multimedia_and_embedding/Responsive_images).
+  - Consider using the `<picture>` element, or the `srcset` & `sizes` attributes to (1) take advantage of new file formats that have better compression, and (2) excercise control over how small or large the requested image is, and lastly (3) apply some art direction in response to the viewport. [Read more about responsive images on MDN](https://developer.mozilla.org/en-US/docs/Learn/HTML/Multimedia_and_embedding/Responsive_images).
 
-- Explore accessibility - the best route here is to apply the appropriate `aria` attributes.
+- Explore accessibility of this UI element more in depth. Apply the appropriate `aria` attributes.
 
-- Add gesture support.
+- Consider changing the unit of measurement for `media queries`, `width`, `height`, `font-size`, `padding`, and everything else from `px` to `rem`!
+
+- Add gesture support. Touch devices are everywhere - not only mobile phones. [Detect touch capabilities with something like Modernizr](https://modernizr.com/), and [use a gesture library like ZingTouch](http://zingchart.github.io/zingtouch/).
+
+- Integrate with a SPA framework! Our JavaScript class should be easily translatable to whichever framework your project uses. [Consider VueJS](https://vuejs.org/)!
---
+++ src/dual-layer-slider/scripts/dual-layer-slider.js
@@ -0,0 +1,141 @@
+"use strict";
+
+var _typeof = typeof Symbol === "function" && typeof Symbol.iterator === "symbol" ? function (obj) { return typeof obj; } : function (obj) { return obj && typeof Symbol === "function" && obj.constructor === Symbol && obj !== Symbol.prototype ? "symbol" : typeof obj; };
+
+var _createClass = function () { function defineProperties(target, props) { for (var i = 0; i < props.length; i++) { var descriptor = props[i]; descriptor.enumerable = descriptor.enumerable || false; descriptor.configurable = true; if ("value" in descriptor) descriptor.writable = true; Object.defineProperty(target, descriptor.key, descriptor); } } return function (Constructor, protoProps, staticProps) { if (protoProps) defineProperties(Constructor.prototype, protoProps); if (staticProps) defineProperties(Constructor, staticProps); return Constructor; }; }();
+
+function _classCallCheck(instance, Constructor) { if (!(instance instanceof Constructor)) { throw new TypeError("Cannot call a class as a function"); } }
+
+var DualLayerSlider = function () {
+	function DualLayerSlider(element) {
+		_classCallCheck(this, DualLayerSlider);
+
+		if (!element) return;
+		this.debug = true;
+		this.log("constructor() called");
+
+		this.element = element;
+		this.fg = element.querySelector(".fg");
+		this.fgSequence = this.fg.querySelector(".sequenced");
+		this.bg = element.querySelector(".bg");
+		this.bgSequence = this.bg.querySelector(".sequenced");
+		this.pagination = this.element.querySelector(".pagination");
+		this.pages = this.pagination.querySelectorAll(".page");
+		this.next = this.pagination.querySelector(".next");
+
+		// Error state: required DOM structure is not provided.
+		if (!this.fg || !this.fgSequence || !this.bg || !this.bgSequence) {
+			return;
+		}
+
+		this.sequenceCount = this.fgSequence.children.length;
+		this.activeIndex = 0;
+		this.translation = 0;
+		this.maxTranslation = this.sequenceCount - 1 * 100;
+
+		this.init();
+	}
+
+	_createClass(DualLayerSlider, [{
+		key: "log",
+		value: function log(message) {
+			if (!message || !this.debug) return;
+
+			if ((typeof message === "undefined" ? "undefined" : _typeof(message)) === _typeof("")) {
+				console.log("Dual Layer Slider: " + message);
+			} else {
+				console.log("Dual Layer Slider:");
+				console.log(message);
+			}
+		}
+	}, {
+		key: "init",
+		value: function init() {
+			this.log("init() called");
+			this.setActiveState();
+			this.handlePagination();
+			this.handleNext();
+		}
+	}, {
+		key: "setActiveState",
+		value: function setActiveState() {
+			this.log("setActiveState() called");
+
+			var activeElements = this.element.querySelectorAll(".-active");
+			[].map.call(activeElements, function (element) {
+				element.classList.remove("-active");
+			});
+
+			var activeFgElement = this.fgSequence.children[this.activeIndex],
+			    activeBgElement = this.bgSequence.children[this.activeIndex],
+			    activePageElement = this.pages[this.activeIndex];
+
+			activeBgElement.classList.add("-active");
+			activeFgElement.classList.add("-active");
+			activePageElement.classList.add("-active");
+		}
+
+		/*
+  	Handles Pagination CLicks
+  	- Set `this.activeIndex`
+  */
+
+	}, {
+		key: "handlePagination",
+		value: function handlePagination() {
+			var _this = this;
+
+			this.log("handlePagination() called");
+
+			[].map.call(this.pages, function (page, index) {
+				page.addEventListener("click", function (e) {
+					_this.activeIndex = index;
+					_this.log("moving to index " + _this.activeIndex);
+					_this.move();
+				});
+			});
+		}
+
+		/*
+  	Handles Next Click
+  	- Set `this.activeIndex`
+  */
+
+	}, {
+		key: "handleNext",
+		value: function handleNext() {
+			var _this2 = this;
+
+			this.log("handleNext() called");
+
+			this.next.addEventListener("click", function (e) {
+				_this2.activeIndex++;
+				if (_this2.activeIndex == _this2.pages.length) _this2.activeIndex = 0;
+				_this2.log("moving to index " + _this2.activeIndex);
+				_this2.move();
+			});
+		}
+	}, {
+		key: "move",
+		value: function move() {
+			this.log("move() called");
+
+			this.setActiveState();
+
+			this.translation = this.activeIndex * -100;
+			this.log("translation " + this.translation);
+			this.bgSequence.style.transform = "translateX(" + this.translation + "%)";
+			this.fgSequence.style.transform = "translateX(" + this.translation + "%)";
+		}
+	}, {
+		key: "reset",
+		value: function reset() {
+			this.log("reset() called");
+
+			this.bgSequence.style.transform = "translateX(" + this.translation + "%)";
+			this.fgSequence.style.transform = "translateX(" + this.translation + "%)";
+		}
+	}]);
+
+	return DualLayerSlider;
+}();
